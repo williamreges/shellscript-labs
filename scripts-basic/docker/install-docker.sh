6ff24df684215e7ea0e8e223a6cd6d197be6c9d6
william-reges <william.reges1986@gmail.com>
2026-01-15 16:23:42 -0300

    update scripts

--- scripts-basic/docker/install-docker.sh
+++ scripts-basic/docker/install-docker.sh
@@ -2,8 +2,8 @@
 set -e
 
 # This script is meant for quick & easy install via:
-#   $ curl -fsSL https://get.docker.com -o get-docker.sh
-#   $ sh get-docker.sh
+#   $ curl -fsSL https://get.docker.com -o install-docker.sh
+#   $ sh install-docker.sh
 #
 # For test builds (ie. release candidates):
 #   $ curl -fsSL https://test.docker.com -o test-docker.sh
